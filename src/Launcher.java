73bd5aa4f009be34aafae9d299b0e991f9622cd9
ziyed.melizi <ziyed.melizi@epita.fr>
2021-05-19 12:36:55 +0200

    add fibo reconition

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,39 @@
 import java.util.Scanner;
 public class Launcher {
 
+static int myfibo(int n){
+    int u0 = 0;
+    int u1 = 1;
+    if (n == 0)
+    {
+        return u0;
+    }
+    if (n == 1)
+    {
+        return u1;
+    }
+    for (int i = 1; i < n; ++i)
+    {
+        u0 = u0 + u1;
+        int t = u0;
+        u0= u1;
+        u1 = t;
+    }
+    return u1;
+}
+
     public static void main(String[] args) {
         System.out.println("un texte de bienvenue");
         Scanner scan = new Scanner(System.in);
         String str = scan.nextLine();
-        while (!str.equals("quit"))
-        {
-            System.out.println("Unknown command");
+        while (!str.equals("quit")) {
+            if (str.equals("fibo")) {
+                System.out.println(myfibo(scan.nextInt()));
+                scan.nextLine();
+            }
+            else {
+                System.out.println("Unknown command");
+            }
             str = scan.nextLine();
         }
     }
